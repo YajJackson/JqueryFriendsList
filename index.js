865f6a2f823bca69da5284f128b49c836b5f22e0
Jay Jackson <jaynickjackson@gmail.com>
2017-12-31 14:35:31 -0500

    added some linting

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 $(() => {
+  
   // add form submit handler
   $('#friend-form').submit(() => {
     $('#friend-list').append(createFriend($('#input').val()))
     $('#input').val('')
   })
 
+  // add new list item
   const createFriend = (name) => 
     $(`<a>${name}</a>`).addClass('list-group-item list-group-item-action list-group-item-dark')
+
 })
