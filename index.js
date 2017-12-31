bef81f20d546799f857862173dff764977b1271f
Jay Jackson <jaynickjackson@gmail.com>
2017-12-31 15:37:39 -0500

    add prevent page refresh

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(() => {
-  
   // add form submit handler
-  $('#friend-form').submit(() => {
+  $('#friend-form').submit((e) => {
+    e.preventDefault()
     $('#friend-list').append(createFriend($('#input').val()))
     $('#input').val('')
   })
 
   // add new list item
   const createFriend = (name) => 
-    $(`<a>${name}</a>`).addClass('list-group-item list-group-item-action list-group-item-dark')
+    $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark')
 
 })
